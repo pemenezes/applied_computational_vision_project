8e42ba9f6f2748810bf3b8841ef0345e97e80af5
Pedro <pepomenezes@hotmail.com>
2026-03-12 16:07:59 -0300

    adição do arquivo da parte conceitual

---
+++ parte_conceitual.py
@@ -0,0 +1,17 @@
+# Arquivo gerado com a finalidade de responder as respostas da parte conceitual do desafio
+
+# 1. Por que a conversão para escala de cinza é importante?
+
+# R: A conversão da imagem para a escala de cinza pe importante porque ela reduz a complexidade 
+# da imagem, facilitando a análise e o processamento. A maioria dos algoritmos de processamento 
+# de imagem e visão computacional trabalha melhor com imagens em escala de cinza, pois eles 
+# se concentram na intensidade dos pixels, em vez de lidar com as informações de cor, o que 
+# pode ser mais complexo e menos relevante para certas tarefas.
+
+# 2. O que o histograma revela sobre a imagem?
+
+# R: O histograma da imagem revela a distribuição das intensidades dos pixels. Ele mostra 
+# quantos pixels possuem cada nível de intensidade, permitindo identificar se a imagem é escura, 
+# clara ou tem um bom contraste. Um histograma concentrado em níveis baixos indica uma imagem 
+# escura, enquanto um histograma concentrado em níveis altos indica uma imagem clara. 
+# Um histograma bem distribuído sugere que a imagem tem um bom contraste.
